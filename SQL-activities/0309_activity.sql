2e3bca83532fc8a7d2ef51c279b06d2af092e2d9
Jay Philip <jayphilipcamillo@gmail.com>
2021-03-09 20:12:42 +0800

    sql 2nd activity

--- SQL-activities/0309_activity.sql
+++ SQL-activities/0309_activity.sql
@@ -1,4 +1,4 @@
 SELECT COUNT(*) as student_count FROM students
-SELECT * FROM students where location = 'Manila'
+SELECT * FROM students WHERE location = 'Manila'
 SELECT AVG(age) as average_age from students 
 SELECT * FROM students ORDER BY age DESC
